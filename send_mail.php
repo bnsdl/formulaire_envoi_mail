0ebdaf762923b02c683de39454588f84d0320d5d
simplon-benoitd <benoitdelboe@hotmail.fr>
2016-04-06 15:39:53 +0200

    fonctionnel

--- send_mail.php
+++ send_mail.php
@@ -1,37 +1,33 @@
 <?php
 
 $to = "benoitdelboe@hotmail.fr";
+$response = [];
 
 //verif email renseigné et format valide
 
-if (isset ($_POST['customer_mail']) && (filter_var($_POST['customer_mail'], FILTER_VALIDATE_EMAIL))){
+if (isset ($_POST['customer_mail']) && (filter_var($_POST['customer_mail'], FILTER_VALIDATE_EMAIL)))
     $header = "From : ".$_POST['customer_mail']."";
-    echo $header;
-}
+
 else
-    echo "email non valide";
+    array_push($response,'Adresse email non valide');
 
 // verif nom renseigné non nul
 
-if (isset($_POST['name']) && $_POST["name"] != ""){
-    $customer_name = $_POST['name'];
-}
-else {
-    echo "missing customer name";
-}
+if (isset($_POST['customer_name']) && $_POST['customer_name'] != "")
+    $customer_name = $_POST['customer_name'];
+
 
 //verif contenu renseigné et non nul
 
 if (isset($_POST['content']) && $_POST['content'] != "")
     $content = $_POST['content'];
-else
-    echo "";
-
-
 
 // si tout est bon alors on envoie le mail
 
-if ((isset($header)) && (isset($customer_name)) && (isset($content)))
+if ((isset($header)) && (isset($customer_name)) && (isset($content))){
     mail($to,$customer_name,$content,$header);
-else
-    header("Location:index.html");
+    array_push($response, "Merci, votre message a bien été envoyé !");
+}
+echo json_encode($response);
+
+
